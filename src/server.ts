4aaa67f7c2287c966c8ecf90cc31b425894952a3
HeiCamq <heidy.quispe@ucb.edu.bo>
2026-05-10 22:10:10 -0400

    feat: integración de main, docker y cors fixes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from "express";
+import cors from "cors"; 
 import { authRouter } from "./interfaces/routes/auth.routes";
 import { pingRouter } from "./interfaces/routes/ping.routes";
 
 const app = express();
+
+app.use(cors()); // (Permite que tu React se conecte)
 app.use(express.json());
 
 app.use(authRouter);
 app.use(pingRouter);
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("Backend funcionando 🚀");
 });
 
