29312f183282e25e5939f61c25acb1bb7e69e31d
valeleuwu28 <valentina.bravo@ucb.edu.bo>
2026-05-21 19:50:46 -0600

    Backend para probarlo en el cele

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,25 @@
 // src/server.ts
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
 
-import { authRouter } from './interfaces/routes/auth.routes';
-import { pingRouter } from './interfaces/routes/ping.routes';
-import { musicRouter } from './interfaces/routes/music.routes';
+import { authRouter } from "./interfaces/routes/auth.routes";
+import { pingRouter } from "./interfaces/routes/ping.routes";
+import { musicRouter } from "./interfaces/routes/music.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send('Backend funcionando 🚀');
+app.get("/", (_req, res) => {
+  res.send("Backend funcionando 🚀");
 });
 
-app.use('/api/auth', authRouter);
-app.use('/api/ping', pingRouter);
-app.use('/api/music', musicRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/ping", pingRouter);
+app.use("/api/music", musicRouter);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
