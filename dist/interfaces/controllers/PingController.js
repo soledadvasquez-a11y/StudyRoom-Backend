28a817ed6e3864657d415cea64f1879a19886d54
HeiCamq <heidy.quispe@ucb.edu.bo>
2026-05-27 18:54:04 -0400

    Ignorar carpeta dist generada

--- dist/interfaces/controllers/PingController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PingController = void 0;
-class PingController {
-    ping(req, res) {
-        res.status(200).json({ status: 'ok', timestamp: new Date() });
-    }
-}
-exports.PingController = PingController;
